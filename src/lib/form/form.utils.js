42d86e3e53ffaea41a3d8c8142e1ebe1d7adbf04
charlo <cmg2512@gmail.com>
2024-09-24 18:18:39 +0200

    feat: change domain server

--- src/lib/form/form.utils.js
+++ src/lib/form/form.utils.js
@@ -8,7 +8,7 @@ export async function handleSubmit(event, executeRecaptcha, callback) {
   const data = new FormData(event.target);
   data.append('token', token);
 
-  fetch('https://mail.asisadenia.es', { method: 'post', body: data }).then(response => {
+  fetch('https://mail.segurosdenia.es', { method: 'post', body: data }).then(response => {
     if (response.ok) {
       toast.success(
         "¡Información enviada con éxito! En breve nuestro equipo se pondrá en contacto contigo."
