8057fba1add4aa01d92869de829dc7983f243806
charlo <cmg2512@gmail.com>
2025-03-27 21:50:04 +0100

    dd

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 import viteCompression from 'vite-plugin-compression';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url'
+import mpa from "vite-plugin-multi-pages";
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCompression()],
+  plugins: [mpa({scanDir: 'src/pages',}), react(), viteCompression()],
   build: {
     rollupOptions: {
       input: {
