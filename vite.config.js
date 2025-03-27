6b5d85025ec7150dff281b1b1677df07158e7c61
charlo <cmg2512@gmail.com>
2025-03-27 21:42:51 +0100

    dd

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import viteCompression from 'vite-plugin-compression';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteCompression()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        insurance: resolve(__dirname, 'insurance-info/index.html'),
+      },
+    },
+  },
 })
