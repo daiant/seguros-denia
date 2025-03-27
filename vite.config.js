ee8cfec387254a239549787012731251841a656e
charlo <cmg2512@gmail.com>
2025-03-27 21:51:23 +0100

    dd

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import viteCompression from 'vite-plugin-compression';
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url'
 import mpa from "vite-plugin-multi-pages";
-const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
