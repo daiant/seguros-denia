10227e0e6c369a2f133a3733202113be38b6e5d7
charlo <cmg2512@gmail.com>
2025-03-27 22:54:44 +0100

    wip

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,9 @@ import viteCompression from 'vite-plugin-compression';
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'dist');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root,
   plugins: [ react(), viteCompression() ],
-  build: {
-    outDir,
-    emptyOutDir: true,
-    rollupOptions: {
-      main: resolve(root, 'index.html'),
-      insurance: resolve(root, 'insurance', 'index.html'),
-    }
-  }
 })
