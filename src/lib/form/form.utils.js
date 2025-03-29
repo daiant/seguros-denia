a18168552c51cf8497e3462a99dbf70c894d32ce
charlo <cmg2512@gmail.com>
2025-03-29 11:02:53 +0100

    wip

--- src/lib/form/form.utils.js
+++ src/lib/form/form.utils.js
@@ -8,7 +8,7 @@ export async function handleSubmit(event, executeRecaptcha, callback) {
   const data = new FormData(event.target);
   data.append('token', token);
 
-  fetch('http://mkkg8oo44sk0044c0o4cgwko.5.196.224.70.sslip.io', { method: 'post', body: data }).then(response => {
+  fetch('https://mail.segurosdenia.es', { method: 'post', body: data }).then(response => {
     if (response.ok) {
       toast.success(
         "¡Información enviada con éxito! En breve nuestro equipo se pondrá en contacto contigo."
